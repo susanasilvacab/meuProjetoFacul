c13a1df272c700f1a7279f7fd5af3256a9c43144
Susana Araujo <susanasilvaacademica@gmail.com>
2026-04-17 20:35:06 -0300

    primeiro commit

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Kassio Felippe</h1>
+      <p>Desenvolvedor Full Stack</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ REDME.md
@@ -0,0 +1,1 @@
+# 1 | Projetinhos da Susana
