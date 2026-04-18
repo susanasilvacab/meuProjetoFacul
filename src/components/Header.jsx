8c79b8a5911f227bc4fc3f7bbe856d188ad1884e
Susana Araujo <susanasilvaacademica@gmail.com>
2026-04-17 21:52:13 -0300

    ultimo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Kassio Felippe</h1>
+      <h1>Susana Silva</h1>
       <p>Desenvolvedor Full Stack</p>
     </header>
   );
